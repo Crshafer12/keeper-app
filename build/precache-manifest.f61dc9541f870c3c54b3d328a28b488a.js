f6c9134c8672c2d7c90e9594ab15e1216784d426
Corey Shafer <crshafer2001@gmail.com>
2023-01-03 11:49:37 -0500

    build

---
+++ build/precache-manifest.f61dc9541f870c3c54b3d328a28b488a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5390b44c23ecfe3f12b219196acdd0f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1113afb7859a4b40ff3",
+    "url": "/static/js/2.cb8305ed.chunk.js"
+  },
+  {
+    "revision": "66d4d9b78f4c98b478c2",
+    "url": "/static/js/main.3559cfb5.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
